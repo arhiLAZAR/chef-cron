35ab3ff77afbb07e0dba39e81c8fdf90b39ed759
Anton Evdokimov <evdokimov@selectel.ru>
2021-02-10 16:18:15 +0300

    Add cookbook foundation

---
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: deploy
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+include_recipe 'delivery-truck::deploy'
---
+++ .delivery/build_cookbook/recipes/security.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: security
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+include_recipe 'delivery-truck::security'
