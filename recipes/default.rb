cad5b068410719b9e369d4a7f419265ce5f04008
Anton Evdokimov <evdokimov@selectel.ru>
2021-02-16 17:07:51 +0300

    Add tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,16 +3,3 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
-
-chef_cron_cronjob 'test_name' do
-  command "echo zzz > /root/cr.txt"
-  time "* * * * *"
-  # action :delete
-end
-
-chef_cron_cronjob 'test_name2' do
-  command "echo zzzX > /root/cr2.txt"
-  time "* * * * *"
-  check_package false
-  check_service false
-end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,14 +3,27 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('cron') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service('cron') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/etc/cron.d/test_job') do
+  its('content') { should match('^############################################################$') }
+  its('content') { should match('^# This file is managed by Chef, and is refreshed regularly #$') }
+  its('content') { should match('^#                       DO NOT EDIT!                       #$') }
+  its('content') { should match('^############################################################$') }
+  its('content') { should match('^## That\'s a testing job$') }
+  its('content') { should match('^MAILTO=tester@example.com$') }
+  its('content') { should match('^# Environment Settings$') }
+  its('content') { should match('^PATH=/bin:/var/lib/tester$') }
+  its('content') { should match('^FOO=bar$') }
+  its('content') { should match('^BAR=com$') }
+  its('content') { should match('^# Job Definition$') }
+  its('content') { should match('^1 2 3 4 5   tester  echo "\${FOO} - \${BAR}" > /tmp/test.txt$') }
 end
