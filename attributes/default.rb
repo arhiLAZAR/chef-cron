24f711b96b8b323a4c9e958b2cc991595a9f391c
Anton Evdokimov <evdokimov@selectel.ru>
2021-02-24 15:59:18 +0300

    Add attributes

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+default['chef_cron']['package_name'] = 'cron'
+default['chef_cron']['service_name'] = 'cron'
+
+# Example:
+# default['chef_cron']['mailto'] = 'tester@example.com'
+# default['chef_cron']['path'] = '/opt/python/bin'
+# default['chef_cron']['environment']['VAR'] = 'value'
+# default['chef_cron']['environment']['FOO'] = 'bar'
+
+# default['chef_cron']['jobs']['test_job']['command'] = 'echo "${VAR} is ${FOO}" > /tmp/test.txt'
+# default['chef_cron']['jobs']['test_job']['time'] = '* * * * *'
+# default['chef_cron']['jobs']['test_job']['user'] = 'tester'
+# default['chef_cron']['jobs']['test_job']['comment'] = 'Testing task'
