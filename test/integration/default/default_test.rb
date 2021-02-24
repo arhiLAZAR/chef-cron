66be95e5ab256c037a600189ab1052138b0e02f5
Anton Evdokimov <evdokimov@selectel.ru>
2021-02-24 16:08:27 +0300

    Fix integration test

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -13,17 +13,19 @@ describe service('cron') do
   it { should be_running }
 end
 
-describe file('/etc/cron.d/test_job') do
+describe file('/etc/cron.d/chef_crontab') do
   its('content') { should match('^############################################################$') }
   its('content') { should match('^# This file is managed by Chef, and is refreshed regularly #$') }
   its('content') { should match('^#                       DO NOT EDIT!                       #$') }
   its('content') { should match('^############################################################$') }
-  its('content') { should match('^## That\'s a testing job$') }
   its('content') { should match('^MAILTO=tester@example.com$') }
   its('content') { should match('^# Environment Settings$') }
-  its('content') { should match('^PATH=/bin:/var/lib/tester$') }
+  its('content') { should match('^PATH=/opt/python/bin$') }
+  its('content') { should match('^VAR=value$') }
   its('content') { should match('^FOO=bar$') }
-  its('content') { should match('^BAR=com$') }
-  its('content') { should match('^# Job Definition$') }
-  its('content') { should match('^1 2 3 4 5   tester  echo "\${FOO} - \${BAR}" > /tmp/test.txt$') }
+  its('content') { should match('^# Jobs Definition$') }
+  its('content') { should match('^# echo_job$') }
+  its('content') { should match('^\* \* \* \* \*   root  echo "\${VAR} is \${FOO}" > /tmp/test.txt$') }
+  its('content') { should match('^# Simple testing job$') }
+  its('content') { should match('^1 2 3 4 5   nobody  echo "Test!"$') }
 end
