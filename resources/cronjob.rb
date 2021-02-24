166c9c6feb451e01bc306bb011bee9ba991f3bd1
Anton Evdokimov <evdokimov@selectel.ru>
2021-02-24 16:02:08 +0300

    Remove package and service from custom resource + fix some spaces

--- resources/cronjob.rb
+++ resources/cronjob.rb
@@ -1,60 +1,36 @@
 # To learn more about Custom Resources, see https://docs.chef.io/custom_resources.html
-resource_name :cronjob
+resource_name :chef_cron_job
 
 # cronfile
-property :file_name,       String, name_property: true
-property :file_owner,      String, default: 'root'
-property :file_group,      String, default: 'root'
-property :file_mode,       String, default: '0644'
-property :cron_dir,        String, default: '/etc/cron.d'
+property :name,          String, default: 'chef_crontab'
+property :file_owner,    String, default: 'root'
+property :file_group,    String, default: 'root'
+property :file_mode,     String, default: '0644'
+property :cron_dir,      String, default: '/etc/cron.d'
 
 # job
-property :command,         String, required: true
-property :time,            String, required: true
-property :user,            String, default: 'root'
-property :comment,         String
-property :mailto,          String
-property :path,            String, default: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-property :environment,     Hash
-
-# package
-property :package_name,    String, default: 'cron'
-property :check_package,   [true, false], default: true
-
-# service
-property :service_name,    String, default: 'cron'
-property :check_service,   [true, false], default: true
+property :jobs,          Hash
+property :default_user,  String, default: 'root'
+property :mailto,        String
+property :path,          String, default: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+property :environment,   Hash
 
 default_action :create
 
 action :create do
-  template "#{new_resource.cron_dir}/#{new_resource.file_name}" do
+  template "#{new_resource.cron_dir}/#{new_resource.name}" do
     source  'cronfile.erb'
     owner   new_resource.file_owner
     group   new_resource.file_group
     mode    new_resource.file_mode
     variables(
-      command:     new_resource.command,
-      time:        new_resource.time,
-      user:        new_resource.user,
-      comment:     new_resource.comment,
-      mailto:      new_resource.mailto,
-      path:        new_resource.path,
-      environment: new_resource.environment
+      jobs:          new_resource.jobs,
+      default_user:  new_resource.default_user,
+      mailto:        new_resource.mailto,
+      path:          new_resource.path,
+      environment:   new_resource.environment
     )
     action  :create
-  end
-
-  if new_resource.check_package
-    apt_package new_resource.package_name do
-      action :install
-    end
-  end
-
-  if new_resource.check_service
-    service new_resource.service_name do
-      action [:enable, :start]
-    end
   end
 end
 
